1187e2a995cec48c30a9df57e4329dc9521a0d44
Cortina17 <diego.cortinagarcia@gmail.com>
2021-12-27 21:24:29 +0100

    Style changes and restart button added

--- main.js
+++ main.js
@@ -1,52 +1,54 @@
-const board_el = document.querySelector('#board');
-const cell_els = document.querySelectorAll('#board .cell');
-const combinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+const board_el = document.querySelector("#board");
+const cell_els = document.querySelectorAll("#board .cell");
 let currentTurn;
+const combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 setUp();
 
 function setUp() {
-    board_el.classList.remove('turn-x', 'turn-o');
+    board_el.classList.remove("turn-x", "turn-o");
 
-    for (let cell of cell_els){
-        cell.classList.remove('x', 'o');
-        cell.addEventListener('click', fillCell, {once: true});
+    for (let cell of cell_els) {
+        cell.classList.remove("x", "o");
+        cell.addEventListener("click", fillCell, { once: true });
     }
 
-    currentTurn = Math.round(Math.random(0, 1)) == 1 ? 'x' : 'o';
-    board_el.classList.add('turn-' + currentTurn);
+    currentTurn = Math.round(Math.random(0, 1)) == 1 ? "x" : "o";
+    board_el.classList.add("turn-" + currentTurn);
 }
 
-function fillCell(){
+function fillCell() {
     this.classList.add(currentTurn);
 
-    if(checkWin()){
-        const restart = confirm(currentTurn.toUpperCase() + " is the winner!! Do you want to play again?");
+    if (checkWin()) {
+        const restart = confirm(
+            currentTurn.toUpperCase() + " is the winner!! Do you want to play again?"
+        );
 
-        if(restart) setUp();
-    }else if(checkDraw()){
+        if (restart) setUp();
+    } else if (checkDraw()) {
         const restart = confirm("It's a draw!! Do you want to play again?");
 
-        if(restart) setUp();
-    } else{
-        currentTurn = currentTurn == 'x' ? 'o' : 'x';
-        board_el.classList.remove('turn-o', 'turn-x');
-        board_el.classList.add('turn-' + currentTurn);
+        if (restart) setUp();
+    } else {
+        currentTurn = currentTurn == "x" ? "o" : "x";
+        board_el.classList.remove("turn-o", "turn-x");
+        board_el.classList.add("turn-" + currentTurn);
     }
 }
 
-function checkWin(){
-    return combinations.some(combination => {
-        return combination.every(c => {
+function checkWin() {
+    return combinations.some((combination) => {
+        return combination.every((c) => {
             if (cell_els[c].classList.contains(currentTurn)) {
                 return true;
             }
@@ -55,10 +57,9 @@ function checkWin(){
     });
 }
 
-function checkDraw(){
-    return [...cell_els].every(c => {
-        if(
-        c.classList.contains('x') || c.classList.contains('o')){
+function checkDraw() {
+    return [...cell_els].every((c) => {
+        if (c.classList.contains("x") || c.classList.contains("o")) {
             return true;
         }
 
